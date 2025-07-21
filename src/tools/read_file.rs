d3073c56af6dfb5a8babcf0156e125a9b37f48ba
truls27a <truls369@gmail.com>
2025-07-21 18:10:32 +0200

    refactor(tools): remove line numbers logic from read file tool

--- src/tools/read_file.rs
+++ src/tools/read_file.rs
@@ -17,22 +17,13 @@ impl Tool for ReadFileTool {
     fn parameters(&self) -> Parameters {
         Parameters {
             param_type: "object".to_string(),
-            properties: HashMap::from([
-                (
-                    "path".to_string(),
-                    Property {
-                        prop_type: "string".to_string(),
-                        description: "The path to the file to read".to_string(),
-                    },
-                ),
-                (
-                    "include_line_numbers".to_string(),
-                    Property {
-                        prop_type: "boolean".to_string(),
-                        description: "Whether to include line numbers in the output".to_string(),
-                    },
-                ),
-            ]),
+            properties: HashMap::from([(
+                "path".to_string(),
+                Property {
+                    prop_type: "string".to_string(),
+                    description: "The path to the file to read".to_string(),
+                },
+            )]),
             required: vec!["path".to_string()],
             additional_properties: false,
         }
@@ -44,25 +35,9 @@ impl Tool for ReadFileTool {
 
     fn call(&self, args: HashMap<String, String>) -> Result<String, String> {
         let path = args.get("path").ok_or("Path is required")?;
-        let include_line_numbers = args
-            .get("include_line_numbers")
-            .map(|s| s.parse::<bool>().unwrap_or(false))
-            .unwrap_or(false);
 
         match fs::read_to_string(path) {
-            Ok(content) => {
-                if include_line_numbers {
-                    let numbered_content = content
-                        .lines()
-                        .enumerate()
-                        .map(|(i, line)| format!("{:4}: {}", i + 1, line))
-                        .collect::<Vec<String>>()
-                        .join("\n");
-                    Ok(numbered_content)
-                } else {
-                    Ok(content)
-                }
-            }
+            Ok(content) => Ok(content),
             Err(e) => Err(format!("Error reading file: '{}': {}", path, e)),
         }
     }
@@ -83,43 +58,6 @@ mod tests {
         let result = tool.call(HashMap::from([("path".to_string(), path.to_string())]));
 
         assert_eq!(result.unwrap(), "Hello, world!");
-
-        fs::remove_file(path).unwrap();
-    }
-
-    #[test]
-    fn call_reads_file_with_line_numbers() {
-        let path = "call_reads_file_with_line_numbers_tmp_file.txt";
-        let content = "First line\nSecond line\nThird line";
-        fs::write(path, content).unwrap();
-
-        let tool = ReadFileTool;
-
-        let result = tool.call(HashMap::from([
-            ("path".to_string(), path.to_string()),
-            ("include_line_numbers".to_string(), "true".to_string()),
-        ]));
-
-        let expected = "   1: First line\n   2: Second line\n   3: Third line";
-        assert_eq!(result.unwrap(), expected);
-
-        fs::remove_file(path).unwrap();
-    }
-
-    #[test]
-    fn call_reads_file_without_line_numbers_when_false() {
-        let path = "call_reads_file_without_line_numbers_tmp_file.txt";
-        let content = "First line\nSecond line";
-        fs::write(path, content).unwrap();
-
-        let tool = ReadFileTool;
-
-        let result = tool.call(HashMap::from([
-            ("path".to_string(), path.to_string()),
-            ("include_line_numbers".to_string(), "false".to_string()),
-        ]));
-
-        assert_eq!(result.unwrap(), "First line\nSecond line");
 
         fs::remove_file(path).unwrap();
     }
